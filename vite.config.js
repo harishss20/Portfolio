2be17ec3543e3f9fb9475e507ea6528270e8a68e
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-10-29 14:41:17 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import path from "path"; // Import the path module
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",
+  base: "/Portfolio",
 });
